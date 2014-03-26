be2fde3dc61b3813df7137e57c284f3800d5b69a
xing <xingxing2d@gmail.com>
2014-03-26 21:44:01 +0800

    version 0.1

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 jquery.trackify
 ===============
 
-jquery scroll and resize trigger with vertical view percentage of element's on window
+jquery scroll and resize trigger a event to element with vertical invisible percentage of element's on window.
+
+This plugin fire the "trackify:update" event with vertical invisible percentage of the element when window's scroll and resize event occurs.
+
+Example:
+
+<div class="featured-overlay">
+</div>
+
+<script>
+$(function(){
+    $('.featured-overlay').trackify();
+    $('.featured-overlay').bind("trackify:update", function(event, elem, percent){
+        $(this).stop();
+        $(this).animate({opacity: percent}, 600);
+    });
+});
+</script>
+
+ ==== OR ====
+
+<script>
+$(function(){
+    $('.featured-overlay').trackify(function(elem, percent){
+        $(elem).stop();
+        $(elem).animate({opacity: percent}, 600);
+    });
+});
+</script>
---
+++ jquery.trackify.js
@@ -0,0 +1,83 @@
+/*
+* title: jquery trackify plugin
+* author: Xing
+* email: xingxing2d@gmail.com
+* date: 2014/03/26
+* Usage:
+
+    <script>
+    $(function(){
+        $('.featured-overlay').trackify();
+        $('.featured-overlay').bind("trackify:update", function(event, elem, percent){
+            $(this).stop();
+            $(this).animate({opacity: percent}, 600);
+        });
+    });
+    </script>
+
+     ==== OR ====
+
+    <script>
+    $(function(){
+        $('.featured-overlay').trackify(function(elem, percentage){
+            $(elem).stop();
+            $(elem).animate({opacity: percent}, 600);
+        });
+    });
+    </script>
+*/
+
+(function ( $ ) {
+  
+  var element = null
+
+  function checkInView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        var rateFromTop = (docViewTop - elemTop)/element.height();
+        var rateFromBottom = (elemBottom - docViewBottom)/element.height();
+
+        rate = getInvisiblePercent(rateFromTop) + getInvisiblePercent(rateFromBottom)
+
+        if(rate <= 0){
+          rate = 0;
+        }
+
+        if(rate >= 1){
+          rate = 1;
+        }
+
+        elem.trigger("trackify:pos", [elem, rate]);
+        return rate;
+      }
+
+  function getInvisiblePercent(percentage) {
+    if(percentage <= 0){
+      percentage = 0;
+    }
+
+    if(percentage >= 1){
+      percentage = 1;
+    }
+
+    return percentage;
+  }
+
+  $.fn.trackify = function(trackFunc) {
+      element = this
+      myFunc = trackFunc;
+
+      $(window).bind("scroll resize", function() {
+
+        percentage = checkInView(element);
+        if(trackFunc){
+          trackFunc(element, percentage);
+        }
+        
+      });
+  };
+})( jQuery );
